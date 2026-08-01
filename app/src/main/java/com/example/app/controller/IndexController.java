a71c02d7cc62e85c0eff9e78c954569916d429f3
Shun100 <denkidenki55@gmail.com>
2026-08-01 22:25:49 +0900

    Add login/logout feature

--- app/src/main/java/com/example/app/controller/IndexController.java
+++ app/src/main/java/com/example/app/controller/IndexController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+  /**
+   * FYI
+   * ログインとログアウト処理を行う@PostMappingが付与されたメソッドはここには無い
+   * Spring Securityがデフォルトのログイン/ログアウトメソッドを用意してくれているのでそれを利用している
+   */
 
   @GetMapping
   public String index() { return "index"; } // view名はtemplatesからの相対パスで記述する
@@ -12,4 +17,6 @@ public class IndexController {
   @GetMapping("/login")
   public String showLoginForm() { return "login"; }
 
+  @GetMapping("/logout")
+  public String showLogoutForm() { return "logout"; }
 }
